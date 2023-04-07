f35f3c93f9848f13f60ede3557046cd33b42aef0
Ryan <mccormick.ryan.95@gmail.com>
2023-04-06 20:42:43 -0400

    Created link to localhost SQL server

--- Repository/MovieRepository.cs
+++ Repository/MovieRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using MovieApi.Models;
+using MySql.Data.MySqlClient;
 
 namespace MovieApi.Repository{
     public class MovieRepository : IMovieRepository{
@@ -10,13 +11,36 @@ namespace MovieApi.Repository{
             new Movie {Name="The Godfather", Genre="Crime", Year= 1972}
 
         };
-
+        private MySqlConnection _connection;
         public MovieRepository(){
-
+            string connectionString="server=localhost;userid=csci330user;password=csci330pass;database=entertainment";
+            _connection = new MySqlConnection(connectionString);
+            _connection.Open();
+        }
+        
+        ~MovieRepository(){
+            _connection.Close();
         }
 
         public IEnumerable<Movie> GetAll(){
-            return movies;
+            var statement = "select * from Movies";
+            var command = new MySqlCommand(statement,_connection);
+            var results = command.ExecuteReader();
+
+            List<Movie> newList = new List<Movie>(20);
+
+            while(results.Read()){
+                Movie m = new Movie{
+                    Name = (string)results[1],
+                    Genre = (string)results[3],
+                    Year = (int)results[2]
+                };
+                newList.Add(m);
+            }
+
+
+            return newList;
+            //return movies;
         }
 
         public Movie GetMovieByName(string name){
